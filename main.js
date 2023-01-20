0bb154c51f64aa27c3def00e3a8217cb2a56993b
Vladislav Surin <vlad_surin@mail.ru>
2023-01-20 23:24:32 +0300

    an array width students is created

--- main.js
+++ main.js
@@ -26,17 +26,59 @@ window.addEventListener('DOMContentLoaded', () => {
         thFaculty = createElement('th', '', 'Факультет'),
         thBrith = createElement('th', '', 'Дата рождения'),
         thYearsOfEducation = createElement('th', '', 'Годы обучения'),
-        tBody = createElement('tbody');
-        
-  renderElement(tHeadTr, thName);
-  renderElement(tHeadTr, thFaculty);
-  renderElement(tHeadTr, thBrith);
-  renderElement(tHeadTr, thYearsOfEducation);
-  renderElement(tHead, tHeadTr);
-  renderElement(table, tHead);
-  renderElement(table, tBody);
-  renderElement(sectionMain, container);
-  renderElement(container, title);
-  renderElement(container, table);
-  renderElement(document.body, sectionMain);
+        tBody = createElement('tbody'),
+        studentsList = [
+          {
+            name: 'Георгий',
+            surname: 'Соболев',
+            patronymic: 'Львович',
+            dateOfBrith: '27.06.1993',
+            startOfStadies: 2020,
+            faculty: 'Психологии'
+          },
+          {
+            name: 'Нина',
+            surname: 'Лаптева',
+            patronymic: 'Артёмовна',
+            dateOfBrith: '23.08.1996',
+            startOfStadies: 2018,
+            faculty: 'Юридический'
+          },
+          {
+            name: 'Софья',
+            surname: 'Меркулова',
+            patronymic: 'Тимуровна',
+            dateOfBrith: '15.07.2003',
+            startOfStadies: 2022,
+            faculty: 'Социологии'
+          },
+          {
+            name: 'Матвей',
+            surname: 'Поляков',
+            patronymic: 'Михайлович',
+            dateOfBrith: '09.10.2002',
+            startOfStadies: 2021,
+            faculty: 'Журналистики'
+          },
+          {
+            name: 'Кирилл',
+            surname: 'Николаев',
+            patronymic: 'Иванович',
+            dateOfBrith: '06.02.2000',
+            startOfStadies: 2019,
+            faculty: 'Юридический'
+          }
+        ];
+
+        renderElement(tHeadTr, thName);
+        renderElement(tHeadTr, thFaculty);
+        renderElement(tHeadTr, thBrith);
+        renderElement(tHeadTr, thYearsOfEducation);
+        renderElement(tHead, tHeadTr);
+        renderElement(table, tHead);
+        renderElement(table, tBody);
+        renderElement(sectionMain, container);
+        renderElement(container, title);
+        renderElement(container, table);
+        renderElement(document.body, sectionMain);
 });
